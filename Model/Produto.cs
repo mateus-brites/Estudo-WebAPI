31af9bae275a6fdff1f5c92b00478624154877bc
Mateus Henrique silva de jesus <mateus.brites@hubconexa.com>
2025-05-24 20:46:53 -0300

    first commit

---
+++ Model/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Model;
+
+public class Produto
+{
+    public Guid ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
